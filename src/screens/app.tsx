a03d7c6cf96e53e67e06f1051d4ae0c13b21d0f2
Daisuke <d.suke.kida@gmail.com>
2025-03-02 12:28:42 +0900

    feat: 一旦コミット

---
+++ src/screens/app.tsx
@@ -0,0 +1,30 @@
+import { type FC } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { registerRootComponent } from "expo";
+import { StatusBar } from "expo-status-bar";
+
+const App: FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: "#fff" }}>Hello, React Native</Text>
+      <Text style={styles.text}>
+        Open up app.tsx to start working on your app!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+});
+
+registerRootComponent(App);
